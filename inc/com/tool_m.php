9928ed4ecf7ef897ec907ec13d2b836fcb3a88d5
Michał Sokołowski <msokolowski@2mnet.pl>
2014-01-22 16:53:57 +0100

    narzedzie dobierania w nowej szacie

--- inc/com/tool_m.php
+++ inc/com/tool_m.php
@@ -65,7 +65,7 @@ if( $F->check_get('search') ) {
    	
    	if( $F->check_get('battery_box') && isset( $battery_box_array[$F->GET['battery_box']] ) 
    		&& strlen($F->GET['battery_box']) > 1 ) {
-   		$where_array['bu.box'] = $battery_typology_array[$F->GET['battery_box']]['text'];
+   		$where_array['bu.box'] = $battery_box_array[$F->GET['battery_box']]['text'];
    	}
    	
    } else {
@@ -76,17 +76,17 @@ if( $F->check_get('search') ) {
    $need_time = (int)$F->GET['battery_time'];
    if( $need_time < 5 ) $need_time = 5; 
 //    echo "   $need_power  $need_time  ";
-   
+
    if( sizeof($where_array) > 0 ) $where_add = ' and ' . db_unroll_conditions($where_array);
    else $where_add = '';
    
    $query = 'SELECT bu.id_ups, bu.model, bu.output_power, bu.output_power_w, bu.cabinet,
    bu.internal_count, bu.internal_capacity, bu.external_count, bu.external_capacity, 
-   bu.box, bu.typology, bu.phase, bu.output_power_w/bu.output_power as normalize_w
+   bu.box, bu.typology, bu.phase, bu.quality, bu.output_power_w/bu.output_power as normalize_w
    FROM `tool_battery_ups` bu
-   WHERE bu.'.$where_denom.' >= ' . db_int($need_power) . '
+   WHERE bu.'.$where_denom.' >= ' . db_int($need_power) . ' ' . $where_add . '
    order by bu.'.$where_denom.'';
-   print_debug($query);
+   //print_debug($query);
    $db_res = db_query($query);
 
    $load_result = db_result_array_full_id($db_res);
@@ -119,34 +119,45 @@ if( $F->check_get('search') ) {
    	}
    }
 
+//    array('quality' => 0, 'under' => , 'over' => );
+   $ups_3cls_array = array('1' => array(), '5' => array(), '10' => array());
+   
    if( $F->not_null($query2_arr) )  {
    	$query2 = implode("\nUNION ALL\n", $query2_arr);
+//    	print_debug($query2);
    	$db_res2 = db_query($query2);
    	$load_result2 = db_result_array_full_id($db_res2);
    	//print_debug($load_result2);
    	foreach( $load_result2 as $id_ups => $ups_data ) {
    		$load_result[$id_ups]['time'] = $ups_data['time'];
    	}
-   	//print_debug($load_result);
-   	foreach($load_result as $id_ups => $ups_data) {
-   		$output_power[$id_ups]  = $ups_data['output_power'];
-   		$time[$id_ups] = $ups_data['time'];
+   	
+   	//$need_time
+   	
+   	
+   	foreach( array_keys($ups_3cls_array) as $quality_search ) {
+   		$time_under = 0;
+   		$time_over = 2400;
+	   	foreach( $load_result as $id_ups => $ups_data ) {
+	   		if( $ups_data['quality'] == $quality_search ) {
+	   			
+	   			//under
+	   			if( $ups_data['time'] < $need_time && 
+						 $ups_data['time'] > $time_under ) {
+	   				$ups_3cls_array[$quality_search]['under'] = $ups_data;
+	   				$time_under =  $ups_data['time'];
+	   			}
+	   			
+	   			//over
+	   			if( $ups_data['time'] > $need_time && 
+						 $ups_data['time'] < $time_over ) {
+	   				$ups_3cls_array[$quality_search]['over'] = $ups_data;
+	   				$time_over =  $ups_data['time'];
+	   			}
+	   		
+	   		}
+	   	}
    	}
-   	array_multisort($output_power, SORT_ASC, $time, SORT_ASC, $load_result);
-   	
-		$nr_ups = array('30' => 0, '60' => 0, '120' => 0, '180' => 0, '240' => 0, '360' => 0, '2400' => 0);
-   	foreach($load_result as $id_ups => $ups_data) {
-   		$time_diff = $ups_data['time'] - $need_time;
-   		foreach($nr_ups as $tt => $tc) { 
-   			if( $time_diff > 0 && $time_diff < $tt ) $nr_ups[$tt]++;
-   		}
-   	}
-   	
-   	$min_time = 2400;
-   	foreach($nr_ups as $tt => $tc) {
-   		if( $tc>4 ) { $min_time = $tt; break; }
-   	}  	
-   	
    } else {
    	$load_result = array();
    }
@@ -175,7 +186,7 @@ $GET_tmp = $F->make_get();
 </table>
 <div class="account_address_container container_subheader">Parametry szukane<div class="icon"></div></div>
 <?php echo $F->draw_form('tool_battery', $F->make_link(CFG_COM_TOOL), 'GET'); ?><br>
-<?php echo $F->draw_hidden_field('com', 'tool'); ?>
+<?php echo $F->draw_hidden_field('com', CFG_COM_TOOL); ?>
 <?php echo $F->draw_hidden_field('sub', 'tool_battery'); ?>
 <table class="pass_table" style="border: 0; width: 100%;">
 	<tr>
@@ -195,31 +206,31 @@ $GET_tmp = $F->make_get();
 	<tr>
 		<td><strong>Typologia:</strong></td>
 		<td colspan="3"><?php 
-		echo $F->draw_pull_down_menu('battery_typology', $battery_typology_array);
+		echo $F->draw_pull_down_menu('battery_typology', $battery_typology_array, $F->GET['battery_typology']);
 		?>
 		</td>
 	</tr>
 	<tr>
 		<td><strong>Obudowa:</strong></td>
 		<td colspan="3"><?php 
-		echo $F->draw_pull_down_menu('battery_box', $battery_box_array);
+		echo $F->draw_pull_down_menu('battery_box', $battery_box_array, $F->GET['battery_box']);
 		?>
 		</td>
 	</tr>
 	<tr>
 		<td><strong>Fazy:</strong></td>
 		<td colspan="3"><?php 
-		echo $F->draw_pull_down_menu('battery_phase', $battery_phase_array);
+		echo $F->draw_pull_down_menu('battery_phase', $battery_phase_array, $F->GET['battery_phase']);
 		?>
 		</td>
 	</tr>
 	<tr>
 		<td><strong>Moc zapasowa:</strong></td>
-		<td colspan="3"><?php echo $F->draw_input_field('additional_power', '0', ' style="width: 30px"'); ?> <strong>[%]</strong></td>
+		<td colspan="3"><?php echo $F->draw_input_field('additional_power', 0, ' style="width: 30px"'); ?> <strong>[%]</strong></td>
 	</tr>
 	<tr><td colspan="4"><hr></td></tr>
 	<tr>
-		<td colspan="4" align="center"><?php echo $F->dynamic_image_submit(Lang::_('Compute'),'COMPUTE'); ?></td>
+		<td colspan="4" align="center"><?php echo $F->dynamic_image_submit(Lang::_('SEARCH'),'SEARCH'); ?></td>
 	</tr>
 </table>
 </div>
@@ -231,40 +242,80 @@ if( $F->not_null($load_result) ) {
 <hr>
 <div class="search_container search_result">
 <?php //print_debug($query) ?>
-<div class="account_address_container container_subheader">Wynik<div class="icon"></div></div>
-<!-- Wymagana moc: <?php echo round($need_power/$wm_sf,2); ?> [kVA]<br><br>-->
-<table class="tableBox" style="border: 0">
-	<tr class="tableBoxHeading">
-		<th><?php echo Lang::_('UPS NAME') . '<br>' . Lang::_('UPS CABINET NAME') ?></th>
-		<th><?php echo Lang::_('OUTPUT_POWER'). '<br>[VA/W]'?></th>
-		<th><?php echo 'Typologia'; ?></th>
-		<th><?php echo 'Obudowa'; ?></th>
-		<th><?php echo 'Fazy'; ?></th>
-		<th><?php echo Lang::_('UPS BACKUP TIME').' [min]' ?></th>
-	</tr>
-	<?php
-	foreach( $load_result as $product ) {
-		if( !isset($product['time']) ) continue;
- 		if( ($product['time']-$need_time) < -5 )  continue;
-		if( $product['time']-$need_time > $min_time )  continue;
-		
-		if( $product['time'] < $need_time ) $time_txt = '<span style="color: red">'.(int)$product['time'].'</span>';
-		else $time_txt = '<span style="color: black">'.(int)$product['time'].'</span>';
-	?>
-	<tr>
-		<td valign="top" width="20%"><?php echo $product['model'] . '<BR>' . $product['cabinet']?></td>
-		<td valign="top" width="10%" align="right"><?php echo $product['output_power'].'/'.$product['output_power_w'] ?></td>
-		<td valign="top" width="20%" align="right"><?php echo strtolower($product['typology']); ?></td>
-		<td valign="top" width="20%" align="right"><?php echo $product['box']; ?></td>
-		<td valign="top" width="5%" align="right"><?php echo $product['phase']; ?></td>
-		<td valign="top" width="5%" align="right"><?php echo $time_txt; ?></td>
-	</tr>
+<div class="account_address_container container_subheader">Wyniki<div class="icon"></div></div>
 <?php 
+$col_array = array('product_column_quality1', 'product_column_quality5', 'product_column_quality10');
+$col_dsp_array = array('1' => 'Tanio', '5' => 'Optymalnie', '10' => 'Najlepiej');
+foreach($col_array as $nr_col => $class_add) {
+echo '<div class="product_columns product_column_'.(int)$nr_col.' '.$class_add.'">';
+	$quality = (int)str_replace('product_column_quality', '', $class_add);
+	
+	echo '<h3>'.$col_dsp_array[$quality].'</h3>';
+	$data = array('data'=> '', 'bottom'=> '', 'footer' => '');
+	if( Framework::not_null($ups_3cls_array[$quality]['over']) ) {
+	  $product = $ups_3cls_array[$quality]['over'];
+	  
+	  if( $product['time'] < $need_time ) $time_txt = '<span style="color: red">'.(int)$product['time'].' min</span>';
+	  else $time_txt = '<span style="color: black">'.(int)$product['time'].' min</span>';
+	  
+	  $data = array(
+			'data'=> 'Model: ' . $product['model'] . "<br>" .
+			(($ups['bu.cabinet']!='')?"Szafka: ".$product['bu.cabinet']:'') . "<br>".
+			'Typologia: ' . strtolower($product['typology']) . "<br>".
+			'Fazy: ' . strtolower($product['phase']) . "<br>",
+		 'bottom'=> 
+			$product['output_power'].'/'.$product['output_power_w'] . ' [VA/W]',
+		 'footer' => $time_txt
+		);
+		echo product_box($data);
+	} else {
+		echo product_box( array('data'=> '<h5>Brak</h5>', 'bottom'=> '-', 'footer' => '-') );
 	}
+	
+	if( Framework::not_null($ups_3cls_array[$quality]['under']) ) {
+	  $product = $ups_3cls_array[$quality]['under'];
+	  
+	  if( $product['time'] < $need_time ) $time_txt = '<span style="color: red">'.(int)$product['time'].' min</span>';
+	  else $time_txt = '<span style="color: black">'.(int)$product['time'].' min</span>';
+	  
+	  $data = array(
+			'data'=> 'Model: ' . $product['model'] . "<br>" .
+			(($ups['bu.cabinet']!='')?"Szafka: ".$product['bu.cabinet']:'') . "<br>".
+			'Typologia: ' . strtolower($product['typology']) . "<br>".
+			'Fazy: ' . strtolower($product['phase']) . "<br>",
+		 'bottom'=> 
+			$product['output_power'].'/'.$product['output_power_w'] . ' [VA/W]',
+		 'footer' => $time_txt
+		);
+		echo product_box($data);
+	} else {
+		echo product_box( array('data'=> '<h5>Brak</h5>', 'bottom'=> '-', 'footer' => '-') );
+	}
+
+echo '</div>';
+}
 ?>
-</table>
 </div>
 <?php
+}
+
+function product_box($data) {
+
+if( !$data ) { }
+
+return '
+<div class="product_in_box">
+	<div class="product_in_box_data">
+	' . $data['data'] . '
+	</div>
+	<div class="product_in_box_bottom">
+	' . $data['bottom'] . '
+	</div>
+	<div class="product_in_box_footer">
+	' . $data['footer'] . '
+	</div>
+</div>
+';
 }
 ?>
 <div class="search_container search_bottom container_bottom"></div>
